e2355ea5878e1e96eba6e021e891f6ce049925bf
farkmarnum <mark@nayya.com>
2022-04-10 21:40:02 -0400

    wip

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,10 +1,38 @@
-import cat from './cat.png';
+import { useEffect, useState } from 'react';
+import cat from '../assets/cat.png';
+
+const getMemory = (): number => {
+  const memory = window.performance.memory;
+  if (memory === undefined) {
+    console.error('Unable to fetch window.performance.memory!')
+    return 0;
+  }
+
+  const { usedJSHeapSize } = memory;
+  return usedJSHeapSize;
+}
 
 const Demo = (): JSX.Element => {
+  const [memory, setMemory] = useState(getMemory())
+
+  useEffect(() => {
+    const interval = setInterval(() => setMemory(getMemory()), 500);
+    return () => {
+      clearInterval(interval);
+    }
+  });
 
   return (
     <div>
+      This image is 3.8MB.
       <img src={cat} className="cat-pic" alt="cat" />
+
+      Clicking this button will update some state and cause a re-render.
+      <br />
+      This, in turn, will cause the nested declaration of the component that renders the above image to be re-created.
+      <br />
+      <br />
+      The total memory in use by JavaScript: {memory}
     </div>
   )
 }
--- src/global.d.ts
+++ src/global.d.ts
@@ -2,3 +2,15 @@ declare module "*.png" {
   const value: any;
   export = value;
 }
+
+// Chrome-specific window.performance:
+interface Performance extends Performance {
+  memory?: {
+    /** The maximum size of the heap, in bytes, that is available to the context. */
+    jsHeapSizeLimit: number;
+    /** The total allocated heap size, in bytes. */
+    totalJSHeapSize: number;
+    /** The currently active segment of JS heap, in bytes. */
+    usedJSHeapSize: number;
+  };
+}
